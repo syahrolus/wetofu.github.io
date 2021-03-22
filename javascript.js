0bc4e58eb0c402f4854f16b42d48c5e096d77d6d
wetofu <syahrolus9@gmail.com>
2021-03-22 11:35:47 -0400

    ganti isi

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let suggestions = [
+    "Kerjasan, Kudus, Jawa Tengah",
+    "Gebog, Kudus, Jawa Tengah",
+    "Jekulo, Kudus, Jawa Tengah",
+    "Colo, Kudus, Jawa Tengah",
+    "GOR Kudus, Kudus, Jawa Tengah",
+    "Natas Angin, Kudus, Jawa Tengah",
+    "Masjid Menara, Kudus, Jawa Tengah"
+];
